3ff85f2e00a0aa6fe0fef1a8a4cb96b6ef44161e
SergioV <sergiovillap93@gmail.com>
2021-08-07 14:18:21 -0300

    "Server - Route & handling of Create,Add,Upda,Del"

--- server/routes/subscriptions.js
+++ server/routes/subscriptions.js
@@ -9,4 +9,63 @@ router.route('/').get((req,res) => {
         .catch(err => res.status(400).json('Error: ' + err))
 })
 
+
+//Receive add subscription in body creating object and handling push to DB
+router.route('/add').post((req, res) => {
+    console.log('Receiving add POST' + req.body.customerId);
+    const customerId = req.body.customerId;
+    const productName = req.body.productName;
+    const domain = req.body.domain;
+    const startDate = Date.parse(req.body.startDate);
+    const durationMonths = req.body.durationMonths;
+
+    //New Object
+    const newSubscription = new Subscription({
+        customerId,
+        productName,
+        domain,
+        startDate,
+        durationMonths,
+    })
+
+    //Push to db and handling of promise
+    newSubscription.save()
+        .then(() => res.status(200).json("Subscription Added"))
+        .catch(err => res.status(400).json('Error: ' + err ))
+});
+
+//Fetch of subscription By id and Get
+router.route('/:id').get((req,res) => {
+    Subscription.findById(req.params.id)
+        .then(() => res.json(subscription))
+        .catch(err => res.status(400).json('Error: '+err))
+});
+
+
+//will delete straight from ID / Might need to refactor later to add verification and error handling
+router.route('/:id').delete((req,res) => {
+    Subscription.findByIdAndDelete(req.params.id)
+        .then(() => res.status(200).json('Subscription Deleted'))
+        .catch(err => res.status(400).json('Error: '+err))
+})
+
+
+//update route will receive POST and save new modified subscription
+router.route('/update/:id').post((req,res) => {
+
+    Subscription.findById(req.params.id)
+        .then(subscription => {
+            subscription.customerId = req.body.customerId;
+    		subscription.productName = req.body.productName;
+    		subscription.domain = req.body.domain;
+    		subscription.startDate = Date.parse(req.body.startDate);
+    		subscription.durationMonths = req.body.durationMonths;
+            
+            subscription.save()
+                .then(() => res.json('Subscription updated'))
+                .catch(err => res.status(400).json('Error: '+err));
+        })
+        .catch(err => res.status(400).json('Error: '+err))
+})
+
 module.exports = router;
